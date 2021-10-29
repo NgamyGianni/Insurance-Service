b2eb38fe8e1eb0a73e9690e576accf3c6f31a363
aziz-abderrahmen <aziz.abderrahmen.mtc@gmail>
2021-10-29 10:12:32 +0200

    First 4 use cases finished

--- db/db_binder.py
+++ db/db_binder.py
@@ -1,6 +1,5 @@
 import sqlite3 as sql
 import json
-import datetime as dt
 
 
 FLAG_CHANGE_SUCCESS = 1
@@ -49,9 +48,9 @@ def searchInsuranceByKey(idInsurance):
     res = dict()
 
     if len(ret) > 0:
-        res["status"] = ret[0][0]
-        res["name"] = ret[0][1]
-        res["amount"] = ret[0][2]
+        res["name"] = ret[0][0]
+        res["amount"] = ret[0][1]
+        res["FLAG"] = ret[0][2]
 
     else:
         return -1
@@ -70,7 +69,7 @@ def createInsurance(idInsurance,n,m):
     if len(ret) > 0:
         return False
     else:
-        req1 = "INSERT INTO \"insurance\" (key,name ,amount,FLAG ) VALUES (\"{0}\",n, m, TRUE)".format(idInsurance)
+        req1 = "INSERT INTO \"insurance\" (key,name ,amount,FLAG ) VALUES (\"{0}\",\"{1}\", \"{2}\", 1)".format(idInsurance,n,m)
         print(req1)
         cursor.execute(req1)
         conn.commit()
